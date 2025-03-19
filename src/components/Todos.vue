aa4a7dc25b25d7f31afde745eef13e987d3d3d45
Killian Reiner <kreiner@enssat.fr>
2025-03-19 11:54:25 +0100

    Init commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <h1>Todo List</h1>
+        <ul>
+            <li v-for="todo in todos" :key="todo.id">{{ todo.title }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            todos: []
+        };
+    },
+    created() {
+        this.fetchTodos();
+    },
+    methods: {
+        async fetchTodos() {
+            try {
+                const response = await fetch('http://localhost:3000/todos');
+                const data = await response.json();
+                this.todos = data;
+            } catch (error) {
+                console.log("salut");x
+                console.error('Error fetching todos:', error);
+            }
+        }
+    }
+};
+</script>
+
+<style scoped>
+h1 {
+    font-size: 2em;
+    margin-bottom: 0.5em;
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    background: #f9f9f9;
+    margin: 0.5em 0;
+    padding: 0.5em;
+    border: 1px solid #ddd;
+}
+</style>
