33647961381c62401ea66f85082573cf09ab78cd
Killian Reiner <kreiner@enssat.fr>
2025-03-21 20:26:55 +0100

    Correction du front

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,42 +1,48 @@
 <template>
-    <div>
-        <form @submit.prevent="submitTodo">
-            <div>
-                <label for="todo">Todo:</label>
-                <input type="text" id="todo" v-model="todoText" required />
-                <button type="submit">Add Todo</button>
-            </div>
-        </form>
-    </div>
+  <div>
+    <form @submit.prevent="submitTodo">
+      <div>
+        <label for="todo">Todo:</label>
+        <input type="text" id="prenom" v-model="prenomText" required />
+        <input type="text" id="todo" v-model="todoText" required />
+        <button type="submit">Add Todo</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            todoText: ''
-        };
-    },
-    methods: {
-        async submitTodo() {
-            try {
-                const response = await fetch('http://localhost:3000/addTodo', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({ text: this.todoText })
-                });
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                this.todoText = '';
-                alert('Todo added successfully!');
-            } catch (error) {
-                console.error('There was a problem with the fetch operation:', error);
-            }
+  data() {
+    return {
+      prenomText: "",
+      todoText: "",
+    };
+  },
+  methods: {
+    async submitTodo() {
+      try {
+        const response = await fetch("https://nginx.reiner.ovh/api/todos", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            prenom: this.prenomText,
+            todo: this.todoText,
+          }),
+        });
+        if (!response.ok) {
+          console.log(response);
+          throw new Error("Network response was not ok");
         }
-    }
+        this.todoText = "";
+        alert("Todo added successfully!");
+      } catch (error) {
+        console.error("There was a problem with the fetch operation:", error);
+      }
+    },
+  },
 };
 </script>
 
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,54 +1,56 @@
 <template>
-    <div>
-        <h1>Todo List</h1>
-        <ul>
-            <li v-for="todo in todos" :key="todo.id">{{ todo.prenom }} doit faire : {{ todo.todo }} | {{ todo.date }}</li>
-        </ul>
-    </div>
+  <div>
+    <h1>Todo List</h1>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.prenom }} doit faire : {{ todo.todo }} | {{ todo.date }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            todos: []
-        };
-    },
-    created() {
-        this.fetchTodos();
-    },
-    methods: {
-        async fetchTodos() {
-            try {
-                const response = await fetch('https://nginx.reiner.ovh/api/todos');
-                const data = await response.json();
-                console.log(data);
-                this.todos = data;
-                for (let i = 0; i < this.todos.length; i++) {
-                    console.log(this.todos[i].prenom);
-                }
-            } catch (error) {
-                console.log("salut");x
-                console.error('Error fetching todos:', error);
-            }
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  created() {
+    this.fetchTodos();
+  },
+  methods: {
+    async fetchTodos() {
+      try {
+        const response = await fetch("http://localhost:3000/api/todos");
+        console.log(response);
+        const data = await response.json();
+        console.log(data);
+        this.todos = data;
+        for (let i = 0; i < this.todos.length; i++) {
+          console.log(this.todos[i].prenom);
         }
-    }
+      } catch (error) {
+        console.error("Error fetching todos:", error);
+      }
+    },
+  },
 };
 </script>
 
 <style scoped>
 h1 {
-    font-size: 2em;
-    margin-bottom: 0.5em;
+  font-size: 2em;
+  margin-bottom: 0.5em;
 }
 ul {
-    list-style-type: none;
-    padding: 0;
+  list-style-type: none;
+  padding: 0;
 }
 li {
-    background: #f9f9f9;
-    margin: 0.5em 0;
-    padding: 0.5em;
-    border: 1px solid #ddd;
+  background: #f9f9f9;
+  margin: 0.5em 0;
+  padding: 0.5em;
+  border: 1px solid #ddd;
 }
 </style>
